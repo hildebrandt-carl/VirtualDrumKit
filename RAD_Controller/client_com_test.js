1634cdfae56cdcaddef4f6c7f3b74bebd911471b
Carl Hildebrandt <hildebrandt.carl@outlook.com>
2017-11-22 15:27:25 -0600

    Updated the server and the controller test code

--- RAD_Controller/client_com_test.js
+++ RAD_Controller/client_com_test.js
@@ -3,7 +3,7 @@ var s = require('net').Socket();
 
 s.connect(4242,'160.119.248.176');
 
-s.write('Web');
+s.write('controller');
 
 s.on('data', function(d){
     console.log(d.toString());
--- RAD_Controller/client_local_test.js
+++ RAD_Controller/client_local_test.js
@@ -3,7 +3,7 @@ var s = require('net').Socket();
 
 s.connect(4242,'127.0.0.1');
 
-s.write('Hello');
+s.write('web');
 
 s.on('data', function(d){
     console.log(d.toString());
